81aff8d0e8220912187890524be50bee2b778534
Jordi <jordivallsplasmx@gmail.com>
2023-02-15 21:33:55 +0100

    Version 1.1

--- app.js
+++ app.js
@@ -2,20 +2,34 @@ let carta = document.querySelectorAll('.img');
 const primeraCarta = carta[0];
 const segonaCarta = carta[1];
 const terceraCarta = carta [2];
-
+let onlyOneOption = false
 
 // segonaCarta.style.removeProperty('background-color');
 
-primeraCarta.addEventListener('click', () => {
-    primeraCarta.style.backgroundColor = 'green';
-    primeraCarta.classList.remove('img:hover');
-});
+while (onlyOneOption == false) {
 
-segonaCarta.addEventListener('click', () => {
-    segonaCarta.style.backgroundColor = 'green';
-});
-
-terceraCarta.addEventListener('click', () => {
-    terceraCarta.style.backgroundColor = 'green';
-});
-
+    switch (onlyOneOption) {
+        case false:
+            primeraCarta.addEventListener('click', () => {
+                primeraCarta.style.backgroundColor = 'green';
+                onlyOneOption = true;
+                console.log(onlyOneOption);
+            });
+            break;
+        case false:
+            segonaCarta.addEventListener('click', () => {
+                segonaCarta.style.backgroundColor = 'green';
+                onlyOneOption = true;
+                console.log(onlyOneOption);
+            });
+            break;
+        case false:
+            terceraCarta.addEventListener('click', () => {
+                terceraCarta.style.backgroundColor = 'green';
+                onlyOneOption = true;
+                console.log(onlyOneOption);
+            });
+            break;
+    }
+    break;
+};
