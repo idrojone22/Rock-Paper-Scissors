1a0b5a9c674c84d096609d3787f8e482922fb874
Jordi <jordivallsplasmx@gmail.com>
2023-02-16 13:03:53 +0100

    Version 1.2

--- app.js
+++ app.js
@@ -1,35 +1,108 @@
+//variables
 let carta = document.querySelectorAll('.img');
 const primeraCarta = carta[0];
 const segonaCarta = carta[1];
 const terceraCarta = carta [2];
-let onlyOneOption = false
+
+let cartaComp = document.querySelectorAll('.img1');
+const primeraCartaComp = cartaComp[0];
+const segonaCartaComp = cartaComp[1];
+const terceraCartaComp = cartaComp[2];
+
+
+let onlyOneOption = true;
+let carta1;
 
 // segonaCarta.style.removeProperty('background-color');
 
-while (onlyOneOption == false) {
+primeraCarta.addEventListener('click', () => {
+    carta1 = 1;
+    solesunacarta()
+    onlyOneOption = false;
+});
+        
+segonaCarta.addEventListener('click', () => {
+    carta1 = 2;
+    solesunacarta()
+    onlyOneOption = false;
+});
 
-    switch (onlyOneOption) {
-        case false:
-            primeraCarta.addEventListener('click', () => {
-                primeraCarta.style.backgroundColor = 'green';
-                onlyOneOption = true;
-                console.log(onlyOneOption);
-            });
-            break;
-        case false:
-            segonaCarta.addEventListener('click', () => {
-                segonaCarta.style.backgroundColor = 'green';
-                onlyOneOption = true;
-                console.log(onlyOneOption);
-            });
-            break;
-        case false:
-            terceraCarta.addEventListener('click', () => {
-                terceraCarta.style.backgroundColor = 'green';
-                onlyOneOption = true;
-                console.log(onlyOneOption);
-            });
-            break;
-    }
-    break;
+terceraCarta.addEventListener('click', () => {
+    carta1 = 3;
+    solesunacarta()
+    onlyOneOption = false;
+});
+
+
+// funciones
+
+function comp() {
+    let numeroRandom = Math.floor(Math.random() * 3);
+    console.log(numeroRandom);
+    if (numeroRandom == 0) {
+        primeraCartaComp.style.backgroundColor = 'red';
+        setTimeout(() => {
+            compararResultat()
+        }, 200);
+        
+    } else if (numeroRandom == 1) {
+        segonaCartaComp.style.backgroundColor = 'red';
+        setTimeout(() => {
+            compararResultat()
+        }, 200);
+    } else {
+        terceraCartaComp.style.backgroundColor = 'red';
+        setTimeout(() => {
+            compararResultat()
+        }, 200);
+    };
+};
+
+function solesunacarta() {
+
+    if (onlyOneOption == true) {
+        if (carta1 == 1) {
+            primeraCarta.style.backgroundColor = 'green';
+            comp();
+        } else if (carta1 == 2) {
+            segonaCarta.style.backgroundColor = 'green';
+            comp();
+        } else {
+            terceraCarta.style.backgroundColor = 'green';
+            comp();
+        };
+    };
+};
+
+function compararResultat() {
+
+    //empates
+    if (primeraCarta.style.backgroundColor == 'green' && primeraCartaComp.style.backgroundColor == 'red') {
+        alert("empate");
+    } else if (segonaCarta.style.backgroundColor == 'green' && segonaCartaComp.style.backgroundColor == 'red') {
+        alert("empate");
+    } else if(terceraCarta.style.backgroundColor == 'green' && terceraCartaComp.style.backgroundColor == 'red') {
+        alert("empate");
+    };
+
+    //piedra victoria
+    if (primeraCarta.style.backgroundColor == 'green' && segonaCartaComp.style.backgroundColor == 'red') {
+        alert('Has perdido');
+    } else if (primeraCarta.style.backgroundColor == 'green' && terceraCartaComp.style.backgroundColor == 'red') {
+        alert ('Has ganado');
+    };
+
+    //papel victoria
+    if (segonaCarta.style.backgroundColor == 'green' && terceraCartaComp.style.backgroundColor == 'red') {
+        alert('Has perdido');
+    } else if (segonaCarta.style.backgroundColor == 'green' && primeraCartaComp.style.backgroundColor == 'red') {
+        alert ('Has ganado');
+    };
+
+    //tijeras victoria
+    if (terceraCarta.style.backgroundColor == 'green' && primeraCartaComp.style.backgroundColor == 'red') {
+        alert('Has perdido');
+    } else if (terceraCarta.style.backgroundColor == 'green' && segonaCartaComp.style.backgroundColor == 'red') {
+        alert ('Has ganado');
+    };
 };
